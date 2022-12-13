c2429d5dce0b427ae2f844743fc1e74b474b1e9c
dgagan2 <andgag@duck.com>
2022-12-13 10:32:43 -0500

    Primer cargue, html terminado

---
+++ index.js
@@ -0,0 +1,107 @@
+let saldos=[{usuario:'Mari',dinero:200},{usuario:'Gera',dinero:290},{usuario:'Maui',dinero:67}]
+let pass=[['Mari',1234],['Gera',12345],['Maui',123456]]
+let valorMin=10
+let valorMax=990
+var userActi
+//Sección Menu principal
+    document.querySelector("#btnUserMari").addEventListener("click", function(){
+        window.userActi='Mari'
+        cambiarSection(".login", ".principal")
+        document.querySelector("#user").innerHTML='Mari'
+        document.querySelector("#loUser").innerHTML=userActi
+    });
+    document.querySelector("#btnUserGera").addEventListener("click", function(){
+        window.userActi='Gera'
+        cambiarSection(".login", ".principal")
+        document.querySelector("#user").innerHTML='Gera'
+        document.querySelector("#loUser").innerHTML=userActi  
+    });
+    document.querySelector("#btnUserMaui").addEventListener("click", function(){
+        window.userActi='Maui'
+        cambiarSection(".login", ".principal")
+        document.querySelector("#user").innerHTML='Maui'
+        document.querySelector("#loUser").innerHTML=userActi       
+    });
+//Sección de login
+    document.querySelector("#btnSignin").addEventListener("click", function(){
+        let passtemp=document.querySelector("#inputPassword").value
+        var acces=consulPass(userActi,passtemp)
+        if(acces==true){
+            cambiarSection(".operacion", ".login")
+        }else{
+            alert('Contraseña invalida')
+        }
+    });
+//Sección de menu operaciones
+    document.querySelector("#btnConsulSaldo").addEventListener("click", function(){
+        cambiarSection(".consultarSaldo", ".operacion")
+        var saldotemp=finByName(userActi).dinero
+        document.querySelector("#saldo").innerHTML=saldotemp
+    });
+    document.querySelector("#btnIngreMonto").addEventListener("click", function(){
+        cambiarSection(".ingresarSaldo", ".operacion")
+    });
+    document.querySelector("#btnRetiMonto").addEventListener("click", function(){
+        cambiarSection(".retirarMonto", ".operacion")
+    });
+//Sección de consultar saldo
+    document.querySelector("#btnReturn").addEventListener("click", function(){
+        cambiarSection(".operacion", ".consultarSaldo")
+    });
+//Boton Ingresar Saldo
+    document.querySelector("#btnIngresarMonto").addEventListener("click", function(){
+        var saldoIng=parseFloat(document.querySelector("#saldoIngresado").value)
+        document.querySelector("#mostarSaldo").innerHTML=saldoIng
+        var saldotemp=parseFloat(finByName(userActi).dinero)
+        var sumaSaldos=saldoIng+saldotemp
+        if(sumaSaldos<valorMax){
+            document.querySelector("#mostarNuevoSaldo").innerHTML=sumaSaldos
+            nuevoSaldo(sumaSaldos)
+        }else{
+            alert("No puede exceder los $"+valorMax+", ingrese otro monto")
+        }
+    });
+//Boton Retirar dinero
+    document.querySelector("#btnRetirarMonto").addEventListener("click", function(){
+        var saldoRet=parseFloat(document.querySelector("#saldoRetirado").value)
+        document.querySelector("#mostrarSaldoRet").innerHTML=saldoRet
+        var saldotemp=parseFloat(finByName(userActi).dinero)
+        var sumaSaldos=saldotemp-saldoRet
+        if(sumaSaldos>valorMin){
+            document.querySelector("#mostrarNueSalRe").innerHTML=sumaSaldos
+            nuevoSaldo(sumaSaldos)
+        }else{
+            alert("No puede dejar en la cuenta menos de $"+valorMin)
+        }
+    });
+//Boton Salir
+document.querySelector("#btnExit").addEventListener("click", function(){
+    location.reload()
+});
+
+function cambiarSection(mostar, ocultar){
+    let mostrarSection=document.querySelector(mostar)
+    mostrarSection.style='display:flex'
+    let ocultarSection=document.querySelector(ocultar)
+    ocultarSection.style="display:none"
+}
+function consulPass(userActi,passtemp){
+    var acceso=false
+    for(var i=0;i<pass.length;i++){
+        if(userActi==pass[i][0]&&passtemp==pass[i][1]){
+            acceso=true
+        }
+    }
+    return acceso
+}
+function finByName(name){
+    return saldos.find(function(x){
+        return x.usuario==name
+    });
+}
+function nuevoSaldo(sumaSaldos){
+    var nuSal=sumaSaldos
+    for(var i=0;i<saldos.length;i++){
+        saldos[i].dinero=nuSal
+    }
+}
